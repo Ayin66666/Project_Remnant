faf86de06f7fcfdbac5965c3633280d950bde55e
Ayin <144647450+Ayin66666@users.noreply.github.com>
2026-02-23 18:35:35 +0900

    26.02.23 작업내역
    1. 세이브 시스템 구현 중
       - 데이터 체크 후 로드 & 생성 기능 구현함
    
    2. 필요한 작업
       - 세이브 용 전용 데이터 클래스 구현 고민해볼 것!
       - 파일 로드 & 유무에 따른 로드 기능 구현 중
       - 생성 기능은 추가 구현 필요 (함수만 선언함)

--- Assets/Scripts/Manager/SaveDataManager.cs
+++ Assets/Scripts/Manager/SaveDataManager.cs
@@ -1,22 +1,35 @@
+using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
 
 public class SaveData
 {
-    // 저장해야하는 데이터
-    // 1. 튜토리얼 여부
-    // 2. 스테이지 클리어 여부
-    // 3. 캐릭터 스테이터스
-    // 4. 편성 데이터
-    // 5. 인벤토리
+    /* 저장해야하는 데이터
+     1. 게임 버전
+     2. 튜토리얼 진행 여부
+     3. 인격 & 에고 & 편성 데이터
+     4. 인벤토리
+     5. 스테이지 클리어 & 진행도
+    */
 
-    public int version;
+    [Header("---버전---")]
+    public string version;
+
+    [Header("---튜토리얼---")]
     public bool playTutorial;
-}
 
-#region Chapter
-#endregion
+    [Header("---인격 & 에고 & 편성---")]
+    public List<OrganizationData> organizationDatas;
+    public List<IdentityInfo> ownedIdentity;
+    public List<EgoInfo> ownedEgo;
+
+    // [Header("---인벤토리---")]
+    // 아직 미구현
+
+    // [Header("---스테이지---")]
+    // public List<StageData> stage;
+}
 
 
 public class SaveDataManager : MonoBehaviour
@@ -24,23 +37,23 @@ public class SaveDataManager : MonoBehaviour
     // 역할 : 세이브 & 로드 매니저
     // 저장 방식 : Json 기반 데이터 저장 / 데이터 경로는 유니티 기본 저장 경로 사용
 
-
-
     [Header("---Setting---")]
     [SerializeField] private string path;
     [SerializeField] private string fileName;
 
     private void Awake()
     {
-        path = Path.Combine(Application.dataPath, fileName);
+        path = Path.Combine(Application.persistentDataPath, fileName);
     }
 
+
+    #region 데이터 저장 & 로드
     /// <summary>
     /// 데이터 존재 여부 체크
     /// </summary>
-    public void CheckData()
+    public bool CheckData()
     {
-
+        return File.Exists(path);
     }
 
     /// <summary>
@@ -49,18 +62,65 @@ public class SaveDataManager : MonoBehaviour
     public void SaveData()
     {
         // 각 매니저에서 데이터 받아오기
-        // 1. 인벤토리 (레벨업 재화 & 동기화 재화) -> Inventory_Manager
-        // 2. 캐릭터 편성 데이터 (인격 & 에고) -> Organization_Manager
-        // 3. 캐릭터 보유 여부 & 스테이터스(레벨 & 동기화) -> CharacterData_Manager
-        // 4. 스테이지 데이터 -> ChapterData_Manager
-        // 5. 튜토리얼 클리어 여부 -> ChapterData_Manager(튜토리얼 종료 후 체크)
+
+        // 데이터 저장
+        SaveData saveData = new SaveData();
     }
 
     /// <summary>
     /// 데이터 로드
     /// </summary>
-    public void LoadData()
+    public SaveData LoadData()
     {
+        if (!File.Exists(path))
+        {
+            // 저장된 데이터가 없을 경우 - 생성
+            Debug.LogError("저장된 데이터가 없습니다.");
+            return NewData();
+        }
 
+        // 저장된 데이터가 있을 경우 - 로드
+        try
+        {
+            // 데이터 로드 시도
+            string json = File.ReadAllText(path);
+
+            // 데이터가 비어있거나 손상된 경우 체크
+            if (string.IsNullOrWhiteSpace(json))
+            {
+                Debug.LogError("세이브 파일이 비어있거나 손상됨");
+                return NewData();
+            }
+            
+            // 데이터 역직렬화
+            SaveData saveData = JsonUtility.FromJson<SaveData>(json);
+
+            // 파일 손상 체크
+            if (saveData == null)
+            {
+                Debug.LogError("역직렬화 실패 - 파일 손상 가능성");
+                return NewData();
+            }
+
+            // 데이터 반환
+            return saveData;
+        }
+        catch (System.Exception e)
+        {
+            // 로드 중 오류 발생 시
+            Debug.LogError("데이터 로드 중 오류 발생: " + e.Message);
+            return NewData();
+        }
     }
+
+    /// <summary>
+    /// 신규 데이터 생성
+    /// </summary>
+    /// <returns></returns>
+    public SaveData NewData()
+    {
+        SaveData data = new SaveData();
+        return data;
+    }
+    #endregion
 }
